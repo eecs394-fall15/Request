06e5eb76f89855a27c32d45f3eaf20acd27a5de2
dongshu <dongs2011@gmail.com>
2015-10-02 21:46:12 -0500

    add reload button

--- app/request/scripts/IndexController.js
+++ app/request/scripts/IndexController.js
@@ -2,20 +2,26 @@ angular
   .module('request')
   .controller("IndexController", function ($scope, Parse, Request, supersonic) {
 
-    $scope.requests = null;
+    $scope.limit = 10;
+    $scope.requests = [];
     $scope.showSpinner = true;
 
-    var query = new Parse.Query(Request);
-    query.descending("createdAt");
-    query.limit(5);
+    var loadRequests = function() {
+      var query = new Parse.Query(Request);
+      query.descending("createdAt");
+      query.limit($scope.limit);
 
-    query.find().then(function(requests) {
-      supersonic.logger.info("Successfully retrieved " + requests.length + " requests.");
-      $scope.$apply( function () {
-        $scope.requests = requests;
-        $scope.showSpinner = false;
+      query.find().then(function(requests) {
+        $scope.$apply( function () {
+          $scope.requests = requests;
+          $scope.showSpinner = false;
+        });
+      },function(error) {
       });
-    },function(error) {
-      supersonic.logger.info("Error: " + error.code + " " + error.message);
-    });
+    }
+
+    $scope.reload = function() {
+      loadRequests();
+    }
+    loadRequests();
 });
