5a8c62a955d0ede4a17040f00afd6ed4d016b61f
Jaiveer Kothari <jaiveerkothari@yahoo.com>
2015-10-03 18:26:41 -0500

    completed logic for accepted requests

--- app/request/scripts/MyAcceptedController.js
+++ app/request/scripts/MyAcceptedController.js
@@ -1,13 +1,13 @@
 angular
   .module('request')
-  .controller("MyAcceptedController", function ($scope, RequestFeed, User, supersonic) {
+  .controller("MyAcceptedController", function ($scope, RequestParse, UserParse, supersonic, Parse) {
     $scope.requests = null;
     $scope.showSpinner = true;
-    supersonic.logger.info("My requests: " + User.current().requests);
+    supersonic.logger.info("My requests: " + UserParse.current().requests);
 
-    var query = new Parse.Query(RequestFeed);
+    var query = new Parse.Query(RequestParse);
     query.descending("createdAt");
-    query.containedIn("accepted_user", User.current().id);
+    query.containedIn("accepted_user", [UserParse.current().id]);
 
     query.find().then(function(requests) {
       supersonic.logger.info("Successfully retrieved " + requests.length + " requests.");
--- app/request/scripts/MyRequestsController.js
+++ app/request/scripts/MyRequestsController.js
@@ -1,13 +1,13 @@
 angular
   .module('request')
-  .controller("MyRequestsController", function ($scope, RequestFeed, User, supersonic) {
+  .controller("MyRequestsController", function ($scope, RequestParse, UserParse, supersonic, Parse) {
     $scope.requests = null;
     $scope.showSpinner = true;
-    supersonic.logger.info("My requests: " + User.current().requests);
+    supersonic.logger.info("My requests: " + UserParse.current().requests);
 
-    var query = new Parse.Query(RequestFeed);
+    var query = new Parse.Query(RequestParse);
     query.descending("createdAt");
-    query.containedIn("objectId", User.current().requests);
+    query.containedIn("objectId", UserParse.current().requests);
 
     query.find().then(function(requests) {
       supersonic.logger.info("Successfully retrieved " + requests.length + " requests.");
