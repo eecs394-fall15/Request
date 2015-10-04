6f59a88075f635e96effa686e815af5caa032b4b
Jaiveer Kothari <jaiveerkothari@yahoo.com>
2015-10-03 20:05:04 -0500

    added new show request page for my requests

---
+++ app/request/scripts/RequestFeed.js
@@ -0,0 +1,15 @@
+angular
+  .module('request')
+  .factory('RequestFeed', function(ParseUtils, Parse) {
+    var request = Parse.Object.extend("Request", {
+      // Instance methods
+    }, {
+      // Class methods
+    });
+
+    var properties = ['title', 'ACL', 'description', 'state', 'location', 'author_user', 'accepted_user'];
+    for (var i = 0; i < properties.length; i++) {
+      ParseUtils.addSetterGetter(request, properties[i]);
+    }
+    return request;
+});
