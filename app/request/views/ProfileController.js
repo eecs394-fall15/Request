26082b7263d4f2189ed8a560f08d676b65affedc
Jaiveer Kothari <jaiveerkothari@yahoo.com>
2015-10-04 16:14:18 -0500

    added a profile tab

---
+++ app/request/views/ProfileController.js
@@ -0,0 +1,37 @@
+angular
+  .module('request')
+  .controller("ProfileController", function ($scope, UserParse, , RequestParse, supersonic, Parse) {
+    $scope.user = null;
+    $scope.showSpinner = true;
+    $scope.dataId = undefined;
+
+    var _refreshViewData = function () {
+      User.find($scope.dataId).then( function (user) {
+        $scope.$apply( function () {
+          $scope.user = user;
+          $scope.showSpinner = false;
+        });
+      });
+    }
+
+    supersonic.ui.views.current.whenVisible( function () {
+      if ( $scope.dataId ) {
+        _refreshViewData();
+      }
+    });
+
+    supersonic.ui.views.current.params.onValue( function (values) {
+      $scope.dataId = values.id;
+      _refreshViewData();
+    });
+
+    
+    $scope.logout = function(id){
+      $scope.showSpinner = true;
+      supersonic.logger.info("User: " + $scope.username + "logging out i think");
+      UserParse.logOut();
+      
+
+      
+    }
+  });
