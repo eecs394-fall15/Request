b301d07eccdca6785a739e19a0827083ffe0c172
dongshu <dongs2011@gmail.com>
2015-10-05 22:59:41 -0500

    add reload button for my requested and my accepted, for #1

--- app/request/scripts/MyAcceptedController.js
+++ app/request/scripts/MyAcceptedController.js
@@ -5,17 +5,25 @@ angular
     $scope.showSpinner = true;
     supersonic.logger.info("My requests: " + UserParse.current().requests);
 
-    var query = new Parse.Query(RequestParse);
-    query.descending("createdAt");
-    query.containedIn("accepted_user", [UserParse.current().id]);
+    var loadMyAccepted = function() {
+      var query = new Parse.Query(RequestParse);
+      query.descending("createdAt");
+      query.containedIn("accepted_user", [UserParse.current().id]);
 
-    query.find().then(function(requests) {
-      supersonic.logger.info("Successfully retrieved " + requests.length + " requests.");
-      $scope.$apply( function () {
-        $scope.requests = requests;
-        $scope.showSpinner = false;
+      query.find().then(function(requests) {
+        supersonic.logger.info("Successfully retrieved " + requests.length + " requests.");
+        $scope.$apply( function () {
+          $scope.requests = requests;
+          $scope.showSpinner = false;
+        });
+      },function(error) {
+        supersonic.logger.info("Error: " + error.code + " " + error.message);
       });
-    },function(error) {
-      supersonic.logger.info("Error: " + error.code + " " + error.message);
-    });
+    }
+
+    $scope.reloadMyAccepted = function() {
+      loadMyAccepted();
+    }
+
+    loadMyAccepted();
 });
