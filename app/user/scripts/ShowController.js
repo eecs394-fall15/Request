39a82b4e4f1e52eb42414ef39b747b5812b247ec
dongshu <dongs2011@gmail.com>
2015-10-28 01:54:39 -0500

    customize user profile view

--- app/user/scripts/ShowController.js
+++ app/user/scripts/ShowController.js
@@ -1,16 +1,43 @@
 angular
   .module('user')
-  .controller("ShowController", function ($scope, User, supersonic) {
+  .controller("ShowController", function ($scope, User, UserParse, supersonic) {
     $scope.user = null;
+    $scope.isAuthor = null;
     $scope.showSpinner = true;
     $scope.dataId = undefined;
+
+    $scope.edit = function(){
+      var editView = new supersonic.ui.View("user#edit?id=" + $scope.user.id);
+      supersonic.ui.modal.show(editView, {animate: true});
+    }
 
     var _refreshViewData = function () {
       User.find($scope.dataId).then( function (user) {
         $scope.$apply( function () {
           $scope.user = user;
           $scope.showSpinner = false;
+          $scope.isAuthor = (UserParse.current().id === user.id);
         });
+
+        if ($scope.isAuthor) {
+          editBtn = new supersonic.ui.NavigationBarButton({
+            onTap: $scope.edit,
+            styleId: "nav-edit"
+          });
+
+          supersonic.ui.navigationBar.update({
+            title: "Request Details",
+            overrideBackButton: false,
+            buttons: {
+              right: [editBtn]
+            }
+          }).then(supersonic.ui.navigationBar.show());
+        } else {
+          supersonic.ui.navigationBar.update({
+          title: "Request Details",
+          overrideBackButton: false,
+          }).then(supersonic.ui.navigationBar.show());
+        }
       });
     };
 
